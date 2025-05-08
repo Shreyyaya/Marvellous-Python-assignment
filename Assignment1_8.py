820c5d5f4cd12753e291153873c92e086b0e8165
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-08 16:53:50 +0530

    Update Assignment1_8.py

--- Assignment1_8.py
+++ Assignment1_8.py
@@ -1,5 +1,5 @@
 print("Enter a number:")
-num = int(input())
+num = int(input())     #num accepts the value as interger
 
-for i in range(num):
-    print("*", end ="")
+for i in range(num):   #here the range is defined as the value of number 
+    print("*", end ="")    #the value of number gets printed in form of '*'
