45f492ec96b44b0667315e9ded4fb8ee027b4b23
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-08 16:50:06 +0530

    Update Assignment1_5.py

--- Assignment1_5.py
+++ Assignment1_5.py
@@ -1,5 +1,5 @@
 def main():
-    print(list(range(10,0,-1)))
+    print(list(range(10,0,-1)))   #range function is used to initialize the list from 10 that goes upto 1 as the displacement value is negative
 
 if __name__ =="__main__":
     main()    
