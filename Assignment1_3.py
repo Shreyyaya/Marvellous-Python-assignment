5c0c0d1fa1d0c8c14b33044599ce23511a16f9cf
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-08 16:47:43 +0530

    Update Assignment1_3.py

--- Assignment1_3.py
+++ Assignment1_3.py
@@ -1,5 +1,5 @@
-def Add(no1, no2):
-    result = no1 + no2
+def Add(no1, no2):     #Add function id defined which accepts 2 parameters
+    result = no1 + no2   #result gives us addition of the accepted parameters
     return result
 
 print("Enter first number:")
@@ -8,5 +8,5 @@ no1 = int(input())
 print("Enter second number:")
 no2 = int(input())
 
-ans = Add(no1,no2)
+ans = Add(no1,no2)     #Add function is used to add the accepted integers from user
 print("The addition is:", ans)
