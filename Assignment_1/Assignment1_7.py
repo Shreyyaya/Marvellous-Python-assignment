172780ae744d5cbfe02b1c1f135afb67136ddcb6
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-08 17:18:36 +0530

    Update Assignment1_7.py

--- Assignment_1/Assignment1_7.py
+++ Assignment_1/Assignment1_7.py
@@ -1,9 +1,9 @@
 
 def Num():
     print("enter the number:")
-    number = int(input())
+    number = int(input())    #number parameter accepts user value in integer datatype
 
-    if(number%5==0):
+    if(number%5==0):     # if the number accepted gives 0 remainder value when divided by 5 then True gets printed
         print("True")
 
     else:
@@ -11,7 +11,6 @@ def Num():
 
 if __name__ =="__main__":
     Num()
-
 
 
 
