a0c679b00d31996723ef42b9a44eefd8462bcac8
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-08 17:18:13 +0530

    Update Assignment1_6.py

--- Assignment_1/Assignment1_6.py
+++ Assignment_1/Assignment1_6.py
@@ -1,9 +1,9 @@
 print("Enter a number:")
-num = int(input())
+num = int(input())    #num is accepted as integer
 
-if (num>0):
+if (num>0):    #if number is greater than 0 then it's positive in nature
     print("number is positive")
-elif (num<0):
+elif (num<0):     #if number is smaller than 0 then it's positive in nature
     print("number is negative")
 else:
     print("number is zero")      
