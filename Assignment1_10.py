5f3c9123fe1c5206b05a9cd64367690f59e31dcf
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-08 17:19:45 +0530

    Delete Assignment1_10.py

--- Assignment1_10.py
+++
@@ -1,6 +0,0 @@
-print("enter a word: ")
-word = input()    #accepts a user defined input
-
-length = len(word)   #len function counts the length of word
-
-print("The length of given word is:", length)  #prints the length of word
