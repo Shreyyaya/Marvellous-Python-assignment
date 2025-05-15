b0b612f22f2a7ff00db6f24702503fb398d72a52
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-15 19:46:05 +0530

    Add files via upload

---
+++ Assignment4/Assignment4_1.py
@@ -0,0 +1,6 @@
+Power = lambda No : No**2    # ** works as exponential
+
+print("Enter a number: ")
+ret = Power(int(input()))
+
+print("Power of given number is:" , ret)
---
+++ Assignment4/Assignment4_2.py
@@ -0,0 +1,10 @@
+Multiplication = lambda X,Y : X*Y
+
+print("Enter first number: ")
+X = int(input())
+
+print("Enter second number: ")
+Y = int(input())
+
+ret = Multiplication(X, Y)
+print("Result is :", ret)
