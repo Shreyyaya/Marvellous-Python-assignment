0bbc21ed4347b780b421c20d5f0614bc3c9de746
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-16 12:49:25 +0530

    Update Assignment3_1.py

--- Assignment_3/Assignment3_1.py
+++ Assignment_3/Assignment3_1.py
@@ -1,23 +1,23 @@
 
 def main():
     print("Enter how many elements :")
-    size = int(input())
+    size = int(input())   #user defined inputs
 
-    Data = list()
+    Data = list()  #empty list is defined
 
     print("Enter the values: ")
     for i in range(size):
         no = int(input())
-        Data.append(no)
+        Data.append(no)  #the inputs gets appended in the empty list named Data
 
     print("Entered elements are: ") 
     for value in Data:
         print([value], end ="")       
-    print()
+    print()   #entered Numbers gets printed
 
     add = 0
     Result = sum(Data)  
-    print("Addition is: ", Result)
+    print("Addition is: ", Result) #sum function is defined to do the addition of all numbers given as inputs
 
 
 if __name__ == "__main__":
