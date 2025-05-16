f91c27e4e71caea507fc813044ec36ebfce51700
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-16 12:46:30 +0530

    Delete Assignment_5 directory

--- Assignment_5/MarvellousNum.py
+++
@@ -1,7 +0,0 @@
-def ChkPrime(num):
-    if (num ==0 or num==1):
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
