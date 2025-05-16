2ac90fdb43673a0880b230c3951b1f839d45671f
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-05-16 12:54:46 +0530

    Update MarvellousNum.py

--- Assignment_3/MarvellousNum.py
+++ Assignment_3/MarvellousNum.py
@@ -1,4 +1,4 @@
-def ChkPrime(num):
+def ChkPrime(num):       #module that contains a function to check whether the number is prime or not
     if (num ==0 or num==1):
         return False
     for i in range(2, int(num ** 0.5) + 1):
