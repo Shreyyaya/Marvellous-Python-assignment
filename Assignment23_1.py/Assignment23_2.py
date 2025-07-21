156f560fc2e2780e20dea08913f7e340efe81efc
Shreyyaya <131597066+Shreyyaya@users.noreply.github.com>
2025-07-21 14:07:50 +0530

    Add files via upload

---
+++ Assignment23_1.py/Assignment23_2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+data = {
+'Name': ['Amit', 'Sagar' ,'Pooja'],
+'Math' : [85,90,78],
+'Science' : [92, 88, 85],
+'English' : [ 78 ,80,82]
+}
+
+df = pd.DataFrame(data)
+print(df.describe())
